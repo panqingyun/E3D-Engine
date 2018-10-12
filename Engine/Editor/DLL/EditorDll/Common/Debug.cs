b2d4d3adf40138fb2b55ec0c6a0cfba63ffd25fe
panqingyun <panqingyun1234@hotmail.com>
2018-10-12 17:03:00 +0800

    新建项目

---
+++ Engine/Editor/DLL/EditorDll/Common/Debug.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3DEditor.Common
+{
+    public delegate void ShowLogDelegate(string log);
+    public class Debug
+    {
+        public static ShowLogDelegate ShowLogHandle;
+
+        public static void Log(string log)
+        {
+            ShowLogHandle?.Invoke(log);
+        }
+
+        public static void Error(string err)
+        {
+            ShowLogHandle?.Invoke("[Error] " + err);
+        }
+
+        public static void Warning(string war)
+        {
+            ShowLogHandle?.Invoke("[Warning] " + war);
+        }
+    }
+}
